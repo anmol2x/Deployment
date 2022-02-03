1d75e953f6ff43fd335b3cda11a77c296f3d086f
EveSpiceapp <EveSpiceapp@gmail.com>
2022-02-03 15:41:07 +0300

    Changed database config to a mongo atlas db

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const MongoClient = require('mongodb').MongoClient;
 
 
-const MONGODB_URI = process.env.ATLAS_URI
+// const MONGODB_URI = process.env.ATLAS_URI
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/loyalRegisteration",
 {
