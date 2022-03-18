11942c17ba38e7bc310b94f1f07c09532b8f8be4
anmol2x <2019420@brunel.ac.uk>
2022-03-18 23:26:41 +0000

    events page

--- src/models/employee.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-let employeeScheme = new mongoose.Schema({
-    name : String,
-    designation : String,
-    salary : Number
-});
-
-module.exports = mongoose.model('Employee', employeeScheme);
