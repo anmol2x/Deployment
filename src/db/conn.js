7d6d2eb39234643e1ccc5b974fe7e973ad87e589
anmol2x <2019420@brunel.ac.uk>
2022-03-19 00:49:40 +0000

    conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 const MongoClient = require('mongodb').MongoClient;
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
-
-
 
 
 // const MONGODB_URI = process.env.ATLAS_URI
@@ -22,12 +15,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/loyalRegi
     console.log("db not connected");
 })
 
-app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
-
-app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({ createdAt: 'desc' })
-    res.render('articles/index', { articles: articles })
-})
 
